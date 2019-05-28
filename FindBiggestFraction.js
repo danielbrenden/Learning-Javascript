bc75191c1ea9e6cedf58fb19dbdb367e4af932bc
Daniel Brenden <danielbrenden0gmail.com>
2019-05-28 16:38:51 -0600

    Added Exercise 1

---
+++ FindBiggestFraction.js
@@ -0,0 +1,9 @@
+// Find the biggest fraction + using ternary operator
+function biggestFraction() {
+	a>b ? console.log("a is bigger at " + a ) : console.log("b is bigger at " + b);
+}
+
+var a = 4/7;
+var b = 3/4;
+
+biggestFraction();
--- README.md
+++
@@ -1,2 +0,0 @@
-# Learning-Javascript
-Exercises in learning Javascript
